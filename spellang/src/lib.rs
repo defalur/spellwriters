163711c8caf21bedf21f2188d265b961b1d581eb
Clément Rebut <clement.rebut@epita.fr>
2020-08-22 19:07:11 +0200

    Started integrating spellang into game

--- spellang/src/lib.rs
+++ spellang/src/lib.rs
@@ -1,6 +1,47 @@
 #[macro_use]
 extern crate lazy_static;
 
+use gdnative::prelude::*;
+
 pub mod spellparser;
 pub mod rune;
 pub mod spellprocessing;
+
+#[derive(gdnative::NativeClass)]
+#[inherit(Node)]
+pub struct SpellCompiler;
+
+#[methods]
+impl SpellCompiler {
+    fn new(_owner: &Node) -> SpellCompiler {
+        SpellCompiler
+    }
+
+    #[export]
+    fn _ready(&self, _owner: &Node) {
+        godot_print!("Hi!");
+    }
+
+    #[export]
+    fn compile_spell(&self, _owner: &Node, input: GodotString) -> Option<spellprocessing::Spell> {
+        let input = &input.to_string();
+        let runes = rune::translate_runes(input);
+        //println!("RuneSeq: {:?}\n\n", runes);
+        //println!("Runes: {}\n\n", input);
+
+        let spellseq = match spellparser::spell_parse(runes){
+            Ok(seq) => seq,
+            _ => {return None;}
+        };
+
+        Some(spellprocessing::create_spell(spellseq))
+    }
+}
+
+fn init(handle: InitHandle) {
+    handle.add_class::<SpellCompiler>();
+}
+
+godot_gdnative_init!();
+godot_nativescript_init!(init);
+godot_gdnative_terminate!();
