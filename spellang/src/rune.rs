e7be961dfcb8e0366b82228322dd07a992ab2a65
Clément Rebut <clement.rebut@epita.fr>
2020-08-25 19:08:58 +0200

    Started refactoring spellang

--- spellang/src/rune.rs
+++ spellang/src/rune.rs
@@ -46,9 +46,9 @@ pub struct RuneCombination {
 }
 
 const RUNE_COMBINATIONS: [RuneCombination; 3] = [
-    RuneCombination{a: RuneMaterial::Frost, b: RuneMaterial::Water, combined: Some(RuneMaterial::Ice)},
-    RuneCombination{a: RuneMaterial::Fire, b: RuneMaterial::Ice, combined: Some(RuneMaterial::Water)},
-    RuneCombination{a: RuneMaterial::Fire, b: RuneMaterial::Frost, combined: None},
+    RuneCombination{a: "frost", b: "water", combined: Some("ice")},
+    RuneCombination{a: "fire", b: "ice", combined: Some("water")},
+    RuneCombination{a: "fire", b: "frost", combined: None},
 ];
 
 pub fn compute_combination(a: &RuneMaterial, b: &RuneMaterial) -> (bool, Option<RuneMaterial>) {
@@ -72,11 +72,11 @@ pub fn compute_combination(a: &RuneMaterial, b: &RuneMaterial) -> (bool, Option<
     (found, result)
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub struct MatRune {
-    pub mat: RuneMaterial,//material type
-    pub main_effect: EffectType,//main effect for the rune
-    pub secondary_effect: Option<EffectType>,//optional secondary effect
+    pub mat: String,//material type
+    pub main_effect: String,//main effect for the rune
+    pub secondary_effect: Option<String>,//optional secondary effect
     pub building_mat: bool,//can be used for statue/wall
     pub protection: bool,//can be used for armor
 }
@@ -88,17 +88,17 @@ pub enum ShapeEffect {
     Protection,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub struct ShapeRune {
-    pub shape: RuneShape,
-    pub trigger: TriggerType,
+    pub shape: String,
+    pub trigger: String,
     pub target_self: bool,
     pub target_ground: bool,
     pub target_entity: bool,
     pub effect: ShapeEffect,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub enum Rune {
     Shape(ShapeRune),
     Material(MatRune),
@@ -106,45 +106,57 @@ pub enum Rune {
 
 impl Rune {
     fn new_shape(
-        shape: RuneShape,
-        trigger: TriggerType,
+        shape: &str,
+        trigger: &str,
         targets: &str,
         effect: ShapeEffect
         ) -> Rune {
         let target_self = targets.chars().any(|c| c == 's');
         let target_ground = targets.chars().any(|c| c == 'g');
         let target_entity = targets.chars().any(|c| c == 'e');
-        Rune::Shape(ShapeRune{shape, trigger, target_self, target_ground, target_entity, effect})
+        Rune::Shape(ShapeRune{
+            shape: shape.to_string(),
+            trigger: trigger.to_string(),
+            target_self,
+            target_ground,
+            target_entity,
+            effect
+        })
     }
 
-    fn new_material(mat: RuneMaterial,
-                    main_effect: EffectType,
-                    secondary_effect: Option<EffectType>,
+    fn new_material(mat: &str,
+                    main_effect: &str,
+                    secondary_effect: Option<String>,
                     building_mat: bool,
                     protection: bool) -> Rune {
-        Rune::Material(MatRune{mat, main_effect, secondary_effect, building_mat, protection})
+        Rune::Material(MatRune{
+            mat: mat.to_string(),
+            main_effect: main_effect.to_string(),
+            secondary_effect,
+            building_mat,
+            protection})
     }
 }
 
 lazy_static! {
     static ref RUNES: [Rune; 12] = [
-        Rune::new_shape(RuneShape::Projectile, TriggerType::Contact, "ge", ShapeEffect::Base),
-        Rune::new_shape(RuneShape::Beam, TriggerType::Contact, "e", ShapeEffect::Base),
-        Rune::new_shape(RuneShape::Statue, TriggerType::Cast, "g", ShapeEffect::Invocation),
-        Rune::new_shape(RuneShape::Area, TriggerType::Cast, "g", ShapeEffect::Base),
-        Rune::new_shape(RuneShape::Focus, TriggerType::Cast, "se", ShapeEffect::Protection),
+        Rune::new_shape("projectile", "contact", "ge", ShapeEffect::Base),
+        Rune::new_shape("beam", "contact", "e", ShapeEffect::Base),
+        Rune::new_shape("statue", "cast", "g", ShapeEffect::Invocation),
+        Rune::new_shape("area", "cast", "g", ShapeEffect::Base),
+        Rune::new_shape("focus", "cast", "se", ShapeEffect::Protection),
         //Rune::new_shape(RuneShape::Wall, TriggerType::Cast, "g", ShapeEffect::Invocation),
-        Rune::new_material(RuneMaterial::Fire, EffectType::Damage, Some(EffectType::Burn), true, true),
-        Rune::new_material(RuneMaterial::Life, EffectType::Heal, None, true, false),
-        Rune::new_material(RuneMaterial::Stone, EffectType::Damage, None, true, true),
-        Rune::new_material(RuneMaterial::Air, EffectType::Damage, Some(EffectType::Bleed), false, false),
-        Rune::new_material(RuneMaterial::Water, EffectType::Amplify, None, true, true),
-        Rune::new_material(RuneMaterial::Frost, EffectType::Freeze, None, false, false),
-        Rune::new_material(RuneMaterial::Ice, EffectType::Damage, Some(EffectType::Freeze), true, true),
+        Rune::new_material("fire", "damage", Some("burn".to_string()), true, true),
+        Rune::new_material("life", "heal", None, true, false),
+        Rune::new_material("stone", "damage", None, true, true),
+        Rune::new_material("air", "damage", Some("bleed".to_string()), false, false),
+        Rune::new_material("water", "amplify", None, true, true),
+        Rune::new_material("frost", "freeze", None, false, false),
+        Rune::new_material("ice", "damage", Some("freeze".to_string()), true, true),
     ];
 }
 
-pub fn find_mat(mat: RuneMaterial) -> Option<MatRune> {
+pub fn find_mat(mat: String) -> Option<MatRune> {
     match RUNES.iter().find(
         |x|
         if let Rune::Material(m) = x {
