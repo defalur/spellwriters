92b12d734f8e83f4673fa3fc7114e381f3edb0e7
Clement Rebut <clement.rebut@epita.fr>
2020-09-03 23:35:08 +0200

    Made area spells to affect an area

--- spellwriters/AreaSpell.gd
+++ spellwriters/AreaSpell.gd
@@ -9,6 +9,13 @@ var max_time = 3
 var cur_time = 0
 var entity_map
 
+const neighbours = [
+	Vector2(-1, 0),
+	Vector2(0, -1),
+	Vector2(1, 0),
+	Vector2(0, 1)
+]
+
 var explosiveness = {
 	"fire": 0,
 	"stone": 1
@@ -17,7 +24,6 @@ var explosiveness = {
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Area Spell ready!")
-	get_node("Particles2D").emitting = true
 	entity_map.update_display()
 	pass # Replace with function body.
 
@@ -31,10 +37,13 @@ func _process(delta):
 
 func change_mat(material):
 	print(material)
-	get_node("Particles2D").texture = load(
-		"res://sprites/particles/" + material + ".png"
-	)
-	get_node("Particles2D").explosiveness = explosiveness[material]
+	for c in get_children():
+		print(c.position)
+		c.emitting = true
+		c.texture = load(
+			"res://sprites/particles/" + material + ".png"
+		)
+		c.explosiveness = explosiveness[material]
 
 func target(origin: Vector2, target: Vector2):
 	position = target
@@ -42,11 +51,18 @@ func target(origin: Vector2, target: Vector2):
 func get_damage():
 	return 1
 
-func get_hits(entities_map):
+func get_hits(entities_map: TileMap):
 	entity_map = entities_map
 
 	var grid_pos = entities_map.world_to_map(position)
+
+	var result = []
 	if entities_map.get_cellv(grid_pos) != -1:
-		return [grid_pos]
-	else:
-		return []
+		result.append(grid_pos)
+	for n in neighbours:
+		if entities_map.get_cellv(grid_pos + n) != -1:
+			result.append(grid_pos + n)
+		var n_pos = entities_map.map_to_world(grid_pos + n) \
+			+ entities_map.cell_size / 2
+
+	return result
